3d8b4f1b571ca754a30addb659e6725c8bc7fd8c
adonovan <adonovan@google.com>
2010-06-14 17:02:19 -0400

    Delete syscalls.

--- newlib-1.17.0/newlib/libc/syscalls/sysclose.c
+++ newlib-1.17.0/newlib/libc/syscalls/sysclose.c
@@ -1,11 +1,1 @@
-/* connector for close */
 
-#include <reent.h>
-#include <unistd.h>
-
-int
-_DEFUN (close, (fd),
-     int fd)
-{
-  return _close_r (_REENT, fd);
-}
--- newlib-1.17.0/newlib/libc/syscalls/sysgetpid.c
+++ newlib-1.17.0/newlib/libc/syscalls/sysgetpid.c
@@ -1,10 +1,1 @@
-/* connector for getpid */
 
-#include <reent.h>
-#include <unistd.h>
-
-int
-_DEFUN_VOID (getpid)
-{
-  return _getpid_r (_REENT);
-}
